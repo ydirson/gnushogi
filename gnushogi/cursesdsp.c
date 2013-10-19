ydirson/gnushogi

commit 6030715dffdadf4402dccf6a8bf9966cfab2be1d

Stop exporting Curses-internal functions to everyone.
Possible since cursesdsp is now self-contained.

gnushogi/cursesdsp.c
@@ -74,13 +74,14 @@ char *DRAW;
 /* FIXME: change this name, puh-leeze! */
 
 static void UpdateCatched(void);
-
+static void DrawPiece(short sq);
+static void ShowScore(short score);
 
 /****************************************
  * Trivial output functions.
  ****************************************/
 
-void
+static void
 ClearEoln(void)
 {
     clrtoeol();
@@ -96,7 +97,7 @@ Curses_ClearScreen(void)
 }
 
 
-void
+static void
 ClearMessage(void)
 {
     gotoXY(TAB, 6);
@@ -104,7 +105,7 @@ ClearMessage(void)
 }
 
 
-void
+static void
 gotoXY(short x, short y)
 {
     move(y - 1, x - 1);
@@ -228,7 +229,7 @@ Curses_ShowPatternCount(short side, short n)
 }
 
 
-void
+static void
 ShowPlayers(void)
 {
     gotoXY(5, ((flag.reverse) ? (5 + 2*NO_ROWS) : 2));
@@ -238,7 +239,7 @@ ShowPlayers(void)
 }
 
 
-void
+static void
 Curses_ShowPrompt(void)
 {
     Curses_ShowSidetoMove();
@@ -299,7 +300,7 @@ Curses_ShowResults(short score, unsigned short *bstline, char ch)
 }
 
 
-void
+static void
 ShowScore(short score)
 {
     gotoXY(TAB, 5);
@@ -721,7 +722,7 @@ Curses_OutputMove(void)
 }
 
 
-void
+static void
 UpdateClocks(void)
 {
     short m, s;
@@ -759,7 +760,7 @@ UpdateClocks(void)
 }
 
 
-void
+static void
 DrawPiece(short sq)
 {
     char y;

gnushogi/gnushogi.h
@@ -1047,9 +1047,6 @@ extern void  ShowCurrentMove(short pnt, short f, short t);
 extern void  ListGame(void);
 extern void  ShowMessage(char *s);
 extern void  ClearScreen(void);
-extern void  gotoXY(short x, short y);
-extern void  ClearEoln(void);
-extern void  DrawPiece(short sq);
 extern void  UpdateClocks(void);
 extern void  DoDebug(void);
 extern void  DoTable(short table[NO_SQUARES]);

gnushogi/cursesdsp.h
@@ -78,17 +78,4 @@ void Curses_TerminateSearch(int sig);
 void Curses_UpdateDisplay(short f, short t, short redraw, short isspec);
 void Curses_help(void);
 
-
-/* The following are only found in cursesdsp.h: */
-
-void ClearEoln(void);
-void ClearMessage(void);
-void DrawPiece(short sq);
-void ShowHeader(void);
-void ShowPlayers(void);
-void ShowScore(short score);
-void UpdateClocks(void);
-void gotoXY(short x, short y);
-
 #endif /* _CURSESDSP_H_ */
-

commit 2afa5512342f9662cb5d8f94ed060dbd69a74c3d

Fix build of --with-curses mode.
Broken by 6030715dffdadf4402dccf6a8bf9966cfab2be1d and not checked soon enough :(

gnushogi/cursesdsp.c
@@ -98,17 +98,17 @@ Curses_ClearScreen(void)
 
 
 static void
-ClearMessage(void)
+gotoXY(short x, short y)
 {
-    gotoXY(TAB, 6);
-    ClearEoln();
+    move(y - 1, x - 1);
 }
 
 
 static void
-gotoXY(short x, short y)
+ClearMessage(void)
 {
-    move(y - 1, x - 1);
+    gotoXY(TAB, 6);
+    ClearEoln();
 }
 
 
@@ -239,7 +239,7 @@ ShowPlayers(void)
 }
 
 
-static void
+void
 Curses_ShowPrompt(void)
 {
     Curses_ShowSidetoMove();

gnushogi/gnushogi.h
@@ -1047,7 +1047,6 @@ extern void  ShowCurrentMove(short pnt, short f, short t);
 extern void  ListGame(void);
 extern void  ShowMessage(char *s);
 extern void  ClearScreen(void);
-extern void  UpdateClocks(void);
 extern void  DoDebug(void);
 extern void  DoTable(short table[NO_SQUARES]);
 extern void  ShowPostnValues(void);