586cbf3d7b87b8bae3154695cc1f6dda396cca7e
Yann Dirson <ydirson@free.fr>
2014-02-09 15:21:05 +0100

    Teach autogen.sh to generate *-pattern.inc, to allow easy crossbuild from git tree.
    Since this requires to build pat2inc on the host, we do a quick configure
    and make, and cleanup afterwards.
    
    Proper support from automake for native tools in a cross setup would be the
    correct way do to things, but we don't have it yet.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 autoreconf -f -i
+# generate files that cannot be made in a cross-build setup yet
+./configure
+make -C gnushogi gnushogi_bootstrap
+make distclean
 
 echo "You may now run ./configure"
